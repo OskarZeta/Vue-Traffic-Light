7e146d42c9a2e4d4ea669855117069cf0baf417f
Juan <dw4h@yandex.ru>
2019-10-07 01:36:39 +0300

    Updated title and constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
-export const TICKS_RED = 10
-export const TICKS_YELLOW = 3
-export const TICKS_GREEN = 15
 export const BLINK_TIME = 3
 export const TICK_LENGTH = 1000
+
+export const LIGHTS_TIMINGS = {
+  red: 10,
+  yellow: 3,
+  green: 15
+}
